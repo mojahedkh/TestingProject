62b85b909de1d706308214a47fab77d86c603213
Oraib Nidal Maswadeh <oraibnidal@gmail.com>
2022-11-16 12:27:18 +0200

    third test

--- src/DriverByOraib.java
+++ src/DriverByOraib.java
@@ -5,6 +5,10 @@ public class DriverByOraib {
     public static void main(String[] args) {
         Test t = new Test("oraib" , 2);
         System.out.println(t.toString());
+
+        //must commit and push after any modification
         System.out.println( " mojahed is editing here ");
+
+        System.out.println("Hello , Everything is good ? ");
     }
 }
